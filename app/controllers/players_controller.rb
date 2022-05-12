2971f7d3e3ed830051f6547693d2c16b901e4493
Daniel Bright <brahyt@gmail.com>
2022-05-12 07:33:09 -0700

    Added players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,6 @@ class PlayersController < ApplicationController
   MAX_PLAYERS = 4
 
   def new
-    debugger
     @team = Team.find_by(identifier: session[:team_identifier])
 
     allowed_players = MAX_PLAYERS - @team.players.count
