b8c2d33d907386a6a10f553b490125556e153f08
dan bright <daniel.bright@drinks.com>
2022-05-13 22:39:35 -0700

    added attributes on models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-
 # Represents a player
 class Player < ApplicationRecord
   belongs_to :team
+  belongs_to :hero
+  belongs_to :aspect
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,5 +4,5 @@
 class Team < ApplicationRecord
   has_many :drone_encounters
   has_many :players
-  accepts_nested_attributes_for :players
+  accepts_nested_attributes_for :players, reject_if: proc { |att| att['hero_id'].blank? }
 end
