43aad384feac091a22f380595e6b727a8c7cd359
Daniel Bright <brahyt@gmail.com>
2023-01-23 22:18:17 -0800

    recolor

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    backgroundImage: {
+      gunmetal: 'linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(43,57,98,1) 35%, rgba(17,17,18,1) 100%)',
+      redmetal: 'linear-gradient(0deg,#a20606,#751616 35%,#ff1313)'
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
